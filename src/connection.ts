513ac6926d0cab5a582f1f736501406c56e51910
fernandolima <nando_filipe@hotmail.com>
2022-06-15 18:42:26 -0300

    set connection local

--- src/connection.ts
+++ src/connection.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/CarShop';
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/CarShop';
+// banco local
+// const MONGO_DB_URL = 'mongodb://localhost:27017/CarShop';
+// avaliador
+const MONGO_DB_URL = 'mongodb://mongodb:27017/CarShop'; 
 
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI
