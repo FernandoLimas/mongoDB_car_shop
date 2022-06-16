b68baa490d2f13b0daf0b791dc63d3808ea16fa7
fernandolima <nando_filipe@hotmail.com>
2022-06-16 10:48:24 -0300

    feat: update index abstract

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -35,6 +35,11 @@ abstract class Controller<T> {
     req: RequestWithBody<T>,
     res: Response<T | ResponseError>,
   ): Promise<typeof res>;
+
+  abstract update(
+    req: RequestWithBody<T>,
+    res: Response<T | ResponseError>,
+  ): Promise<typeof res>;
 }
 
 export default Controller;
