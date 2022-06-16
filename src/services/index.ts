18d8045fda24bdd908ac3d390447734ccff80d5c
fernandolima <nando_filipe@hotmail.com>
2022-06-16 11:43:41 -0300

    feat: delete service

--- src/services/index.ts
+++ src/services/index.ts
@@ -23,6 +23,10 @@ abstract class Service<T> {
   public async update(id: string, body: T): Promise<T | null | ServiceError> {
     return this.model.update(id, body);
   }
+
+  public async delete(id: string): Promise<T | null | ServiceError> {
+    return this.model.delete(id);
+  }
 }
 
 export default Service;
