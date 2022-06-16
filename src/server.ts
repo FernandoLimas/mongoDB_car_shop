c56825001302de8435f21f48462d5ee254a89200
fernandolima <nando_filipe@hotmail.com>
2022-06-16 12:39:07 -0300

    feat: server add router motocyle

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,21 @@
 import App from './app';
-import CarRouter from './routes/router';
 import CarController from './controllers/carController';
 import { Car } from './interfaces/CarInterface';
+import MotoController from './controllers/motoController';
+import RoutesGenerics from './routes/router';
+import { Motorcycle } from './interfaces/MotorcycleInterface';
 
 const server = new App();
 
 const carController = new CarController();
-
-const carRouter = new CarRouter<Car>();
-
+const carRouter = new RoutesGenerics<Car>();
 carRouter.addRoute(carController);
 
+const motoController = new MotoController();
+const motoRoute = new RoutesGenerics<Motorcycle>();
+motoRoute.addRoute(motoController);
+
 server.addRouter(carRouter.router);
+server.addRouter(motoRoute.router);
 
 export default server;
