78ba7055c2334e86dd5cea97ef398582db7b8341
fernandolima <nando_filipe@hotmail.com>
2022-06-16 15:25:23 -0300

    feat: mocks test

---
+++ src/tests/unit/helpers/motoMock.ts
@@ -0,0 +1,48 @@
+export const allMotorcyclesMock = [
+	{
+		"_id": "62911f8664d2dc424169eb18",
+		"model": "Honda 200cc",
+		"year": 1995,
+		"color": "Black",
+		"buyValue": 17000,
+		"category": "Street",
+		"engineCapacity": 2000
+	},
+	{
+		"_id": "62911f8964d2dc424169eb1a",
+		"model": "Honda 200cc",
+		"year": 1995,
+		"color": "Black",
+		"buyValue": 17000,
+		"category": "Street",
+		"engineCapacity": 2000
+	},
+	{
+		"_id": "62911f8964d2dc424169eb1c",
+		"model": "Honda 200cc",
+		"year": 1995,
+		"color": "Black",
+		"buyValue": 17000,
+		"category": "Street",
+		"engineCapacity": 2000
+	}
+]
+
+export const oneMotorcycleMock = {
+	"_id": "62911f8664d2dc424169eb18",
+	"model": "Honda 200cc",
+	"year": 1995,
+	"color": "Black",
+	"buyValue": 17000,
+	"category": "Street",
+	"engineCapacity": 2000
+}
+
+export const newMotorcycleMock = {
+	"model": "Honda 200cc",
+	"year": 1995,
+	"color": "Black",
+	"buyValue": 17000,
+	"category": "Street",
+	"engineCapacity": 2000
+}
