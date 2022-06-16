aef6e3a9f6a1d722ad0399d79d81ff3c975c078a
fernandolima <nando_filipe@hotmail.com>
2022-06-16 18:50:47 -0300

    test

--- src/connection.ts
+++ src/connection.ts
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
 // banco local
-// const MONGO_DB_URL = 'mongodb://localhost:27017/CarShop';
+const MONGO_DB_URL = 'mongodb://localhost:27017/CarShop';
 // avaliador
-const MONGO_DB_URL = 'mongodb://mongodb:27017/CarShop';
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/CarShop';
 
 const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI
