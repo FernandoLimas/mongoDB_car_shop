efc9fb38cc7159dd9482ba06f87d7d0bee9718f1
fernandolima <nando_filipe@hotmail.com>
2022-06-16 15:25:04 -0300

    feat: test models

---
+++ src/tests/unit/helpers/carMock.ts
@@ -0,0 +1,57 @@
+export const getAllCarsMock = [
+	{
+		"_id": "628f7b9ee2be6772f5e86099",
+		"model": "Marea Turno",
+		"year": 1995,
+		"color": "Black",
+		"buyValue": 17000,
+		"seatsQty": 4,
+		"doorsQty": 4
+	},
+	{
+		"_id": "628f7ba0e2be6772f5e8609b",
+		"model": "Marea Turno",
+		"year": 1995,
+		"color": "Black",
+		"buyValue": 17000,
+		"seatsQty": 4,
+		"doorsQty": 4
+	},
+	{
+		"_id": "628f7ba1e2be6772f5e8609d",
+		"model": "Marea Turno",
+		"year": 1995,
+		"color": "Black",
+		"buyValue": 17000,
+		"seatsQty": 4,
+		"doorsQty": 4
+	},
+	{
+		"_id": "628f7ba1e2be6772f5e8609f",
+		"model": "Marea Turno",
+		"year": 1995,
+		"color": "Black",
+		"buyValue": 17000,
+		"seatsQty": 4,
+		"doorsQty": 4
+	}
+]
+
+export const newCarMock = {
+  "model": "Marea Turno",
+  "year": 1995,
+  "color": "Black",
+  "buyValue": 17000,
+  "seatsQty": 4,
+  "doorsQty": 4
+}
+
+export const carMock = {
+  "_id": "628fafea3b94a2894ec3f9e7",
+  "model": "Marea Turno",
+  "year": 1995,
+  "color": "Black",
+  "buyValue": 17000,
+  "seatsQty": 4,
+  "doorsQty": 4
+}
