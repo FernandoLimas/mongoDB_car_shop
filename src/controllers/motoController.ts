22596f07514609daaafba6d28145fc65b75d1d45
fernandolima <nando_filipe@hotmail.com>
2022-06-16 12:39:26 -0300

    feat: moto controller

---
+++ src/controllers/motoController.ts
@@ -0,0 +1,116 @@
+import { Response } from 'express';
+import Controller, { RequestWithBody, ResponseError } from '.';
+import { Motorcycle } from '../interfaces/MotorcycleInterface';
+import MotoService from '../services/MotoService';
+
+class MotoController extends Controller<Motorcycle> {
+  private $route: string;
+  
+  constructor(
+    service = new MotoService(),
+    route = '/motorcycles',
+  ) {
+    super(service);
+    this.$route = route;
+  }
+
+  get route() { return this.$route; }
+
+  public create = async (
+    req: RequestWithBody<Motorcycle>,
+    res: Response<Motorcycle | ResponseError>,
+  ): Promise<typeof res> => {
+    const newMoto = await this.service.create(req.body);
+    if (!newMoto) {
+      return res.status(400).json({ error: this.errors.badRequest });
+    }
+    
+    if ('error' in newMoto) return res.status(400).json(newMoto);
+    
+    return res.status(201).json(newMoto);
+  };
+
+  public getAll = async (
+    _req: RequestWithBody<Motorcycle>,
+    res: Response<Motorcycle[] | ResponseError>,
+  ): Promise<typeof res> => {
+    const allMotos = await this.service.getAll();
+    if (!allMotos) {
+      return res.status(400).json({ error: this.errors.badRequest });
+    }
+    
+    if ('error' in allMotos) return res.status(400).json(allMotos);
+    
+    return res.status(200).json(allMotos);
+  };
+
+  public getById = async (
+    req: RequestWithBody<Motorcycle>,
+    res: Response<Motorcycle | ResponseError>,
+  ): Promise<typeof res> => {
+    const { id } = req.params;
+    const hex = /[0-9A-Fa-f]{24}/g;
+
+    if (!hex.test(id)) {
+      return res.status(400)
+        .json({ error: this.errors.requiredId });
+    }
+    const motoById = await this.service.getById(id);
+    
+    if (!motoById) {
+      return res.status(404).json({ error: this.errors.notFound });
+    }
+    
+    return res.status(200).json(motoById);
+  };
+
+  public update = async (
+    req: RequestWithBody<Motorcycle>,
+    res: Response<Motorcycle | ResponseError>,
+  ): Promise<typeof res> => {
+    const { id } = req.params;
+
+    const hex = /[0-9A-Fa-f]{24}/g;
+
+    if (!hex.test(id)) {      
+      return res.status(400)
+        .json({ error: this.errors.requiredId });
+    }
+
+    if (Object.keys(req.body).length === 0) {
+      return res
+        .status(400).json({ error: this.errors.badRequest });
+    }
+
+    const updateMoto = await this.service.update(id, req.body);
+
+    if (!updateMoto) {
+      return res.status(404).json({ error: this.errors.notFound });
+    }
+    
+    return res.status(200).json(updateMoto);
+  };
+
+  public delete = async (
+    req: RequestWithBody<Motorcycle>,
+    res: Response<Motorcycle | ResponseError>,
+  ): Promise<typeof res> => {
+    const { id } = req.params;
+    const hex = /[0-9A-Fa-f]{24}/g;
+
+    if (!hex.test(id)) {      
+      return res.status(400)
+        .json({ error: this.errors.requiredId });
+    }
+
+    const removedMoto = await this.service.delete(id);
+
+    if (!removedMoto) {
+      return res.status(404).json({ error: this.errors.notFound });
+    }
+    
+    return res.status(204).json(removedMoto);
+  };
+}
+
+export default MotoController;
