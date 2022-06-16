981474448cdb0f4d54ac50f557b5c56dee061f6c
fernandolima <nando_filipe@hotmail.com>
2022-06-16 02:14:28 -0300

    feat: /cars index

---
+++ src/controllers/index.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+import Service from '../services/index';
+
+export type ResponseError = {
+  error: unknown;
+};
+
+export interface RequestWithBody<T> extends Request {
+  body: T;
+}
+
+enum ControllerErrors {
+  internal = 'Internal Server Error',
+  notFound = 'Object not found',
+  requiredId = 'Id is required',
+  badRequest = 'Bad request',
+}
+
+abstract class Controller<T> {
+  abstract route: string;
+
+  protected errors = ControllerErrors;
+
+  constructor(protected service: Service<T>) {}
+
+  abstract create(
+    req: RequestWithBody<T>,
+    res: Response<T | ResponseError>,
+  ): Promise<typeof res>;
+}
+
+export default Controller;
