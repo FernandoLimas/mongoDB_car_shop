517adb3073d68333d4f04c865098b51498ec7957
fernandolima <nando_filipe@hotmail.com>
2022-06-16 12:02:38 -0300

    feat: VehicleInterface

--- src/interfaces/VehicleInterface.ts
+++ src/interfaces/VehicleInterface.ts
@@ -1,13 +1,38 @@
 import { z } from 'zod';
 
-const IVehicle = z.object({
-  model: z.string().min(3),
-  year: z.number().gte(1900).lte(2022),
-  color: z.string().min(3),
-  status: z.boolean().optional(),
-  buyValue: z.number().int(),
+const vehicleInterface = z.object({
+  model: z.string({
+    required_error: 'Model is required',
+    invalid_type_error: 'Model must be a string',
+  }).min(3, { message: 'Model must be 3 or more characters long' }),
+  year: z
+    .number({
+      required_error: 'Year is required',
+      invalid_type_error: 'Year must be a number',
+    })
+    .min(
+      1900,
+      { message: 'Year must be equal or greater than 1900' },
+    )
+    .max(
+      2022,
+      { message: 'Year must be equal or less than 2022' },
+    ),
+  color: z.string({
+    required_error: 'Color is required',
+    invalid_type_error: 'Color must be a string',
+  }).min(3, { message: 'Color must be 3 or more characters long' }),
+  status: z.optional(z.boolean({
+    required_error: 'Status is required',
+    invalid_type_error: 'Status must be a boolean',
+  })),
+  buyValue: z.number({
+    required_error: 'Buy value is required',
+    invalid_type_error: 'Buy value must be a number',
+  }).int({
+    message: 'Buy value must be an integer',
+  }),
 });
+type Vehicle = z.infer<typeof vehicleInterface>;
 
-type Vehicle = z.infer<typeof IVehicle>;
-
-export { Vehicle, IVehicle };
+export { Vehicle, vehicleInterface };
