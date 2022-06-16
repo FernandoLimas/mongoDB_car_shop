f8bd4f757347e3f190a2b1f3698571f8a6aa60c7
fernandolima <nando_filipe@hotmail.com>
2022-06-16 02:15:23 -0300

    feat: /cars MongoModel

---
+++ src/models/MongoModel.ts
@@ -0,0 +1,19 @@
+import { Model as M } from 'mongoose';
+import { Model } from '../interfaces/ModelInterface';
+
+export default abstract class MongoModel<T> implements Model<T> {
+  constructor(protected model: M<T>) {}
+
+  create = async (obj: T): Promise<T> => this.model.create({ ...obj });
+
+  read = async (): Promise<T[]> => this.model.find();
+
+  readOne = async (id: string): Promise<T | null> =>
+    this.model.findOne({ _id: id });
+
+  update = async (id: string, obj: T): Promise<T | null> =>
+    this.model.findOneAndUpdate({ _id: id }, { ...obj });
+
+  delete = async (id: string): Promise<T | null> =>
+    this.model.remove({ _id: id });
+}
