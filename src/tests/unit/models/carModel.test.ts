cf9231d5f42023b86aef4dd958e1a3dd8e859dfc
fernandowin <nando_filipe@hotmail.com>
2022-06-21 15:35:39 -0300

    test carModel

--- src/tests/unit/models/carModel.test.ts
+++ src/tests/unit/models/carModel.test.ts
@@ -1,6 +1,6 @@
 import CarModel from "../../../models/CarModel";
 import { expect } from "chai";
-import { allCarsMock } from "../helpers/carMock";
+import { allCarsMock, oneCarMock, newCarMock } from "../helpers/carMock";
 import Sinon from "sinon";
 
 describe('Test CarModel', () => {
@@ -21,8 +21,29 @@ describe('Test CarModel', () => {
             expect(getCars).to.be.an('array');
             expect(getCars).to.have.lengthOf(allCarsMock.length);
             expect(getCars).to.deep.equal(allCarsMock);
-        }
-        )
+        })
 
     })
+
+    describe('Get new car', () => {
+        const modelcar = new CarModel();
+
+        before(() => {
+            Sinon.stub(modelcar, "create").resolves(oneCarMock);
+            
+        })
+
+        after(() => {
+            Sinon.restore();
+        })
+
+        it('should return new car', async () => {
+            const newCar = await modelcar.create(newCarMock);
+            expect(newCar).to.be.an('object');
+            expect(newCar).to.deep.equal(oneCarMock);
+            expect(newCar).to.have.property('color')
+        })
+
+    })
+
 })
