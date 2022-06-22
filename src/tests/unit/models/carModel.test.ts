73413aca61186b6e065fdbb4907834b688709ee6
fernandowin <nando_filipe@hotmail.com>
2022-06-22 01:38:19 -0300

    test car model 90%

--- src/tests/unit/models/carModel.test.ts
+++ src/tests/unit/models/carModel.test.ts
@@ -1,49 +1,64 @@
 import CarModel from "../../../models/CarModel";
 import { expect } from "chai";
-import { allCarsMock, oneCarMock, newCarMock } from "../helpers/carMock";
+import { allCarsMock, oneCarMock, newCarMock, updatedCarMock } from "../helpers/carMock";
+import mongoose from "mongoose";
 import Sinon from "sinon";
 
+
+
+
 describe('Test CarModel', () => {
-    describe('Get all cars', () => {
-        const modelcar = new CarModel();
+  const modelCar = new CarModel();
+  const _id = '628fafea3b94a2894ec3f9e7';
 
-        before(() => {
-            Sinon.stub(modelcar, "read").resolves(allCarsMock);
-            
-        })
+    before(async (done) => {
+        mongoose.connect('mongodb://localhost/testDatabase');
+        const db = mongoose.connection;
+        done()
+    });
 
-        after(() => {
-            Sinon.restore();
-        })
+    after((done) => {
+        mongoose.connection.db.dropDatabase(() => {
+            mongoose.connection.close();
+            done();
+        });
+    });
 
-        it('should return all cars', async () => {
-            const getCars = await modelcar.read();
-            expect(getCars).to.be.an('array');
-            expect(getCars).to.have.lengthOf(allCarsMock.length);
-            expect(getCars).to.deep.equal(allCarsMock);
-        })
-
-    })
-
-    describe('Get new car', () => {
-        const modelcar = new CarModel();
-
-        before(() => {
-            Sinon.stub(modelcar, "create").resolves(oneCarMock);
-            
-        })
-
-        after(() => {
-            Sinon.restore();
-        })
+    describe('Car Model', () => {
+        it('should exist modelCar', () => {
+            expect(modelCar).to.exist;
+        });
 
         it('should return new car', async () => {
-            const newCar = await modelcar.create(newCarMock);
-            expect(newCar).to.be.an('object');
-            expect(newCar).to.deep.equal(oneCarMock);
-            expect(newCar).to.have.property('color')
+        
+            const newCar = await modelCar.create(newCarMock);
+            expect(newCar._id).to.exist;
+            expect(newCar).to.have.property('color');
+            expect(newCar).to.have.property('model');
+            expect(newCar).to.have.property('year');
+            expect(newCar).to.have.property('buyValue');
+            expect(newCar).to.have.property('doorsQty');
+            expect(newCar).to.have.property('seatsQty');
         })
 
-    })
+        it('should get by id', async () => {
+            const getCar = await modelCar.readOne(_id);
+            expect(getCar).to.be.a('string');
+        })
 
-})
+
+        it('should return all cars', async () => {
+            const getCars = await modelCar.read();
+            expect(getCars).to.be.an('array');
+        })
+
+        it('should update car', async () => {
+            
+            const result = await modelCar.create(newCarMock);
+
+            const updated = await modelCar.update(_id, updatedCarMock) as any;
+            expect(_id, updated).to.exist;
+        });
+
+    });
+});
