2973cf5a21315ffd0f24350aa986b706a70109ab
fernandowin <nando_filipe@hotmail.com>
2022-06-23 16:04:57 -0300

    tests carController

--- src/tests/unit/controllers/carController.test.ts
+++ src/tests/unit/controllers/carController.test.ts
@@ -115,5 +115,44 @@ describe('Test carController', () => {
           return res.body;
       });
     });
+
+    it('should return status 400', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .get(`/cars/${oneCarMock.model}`)
+        .then(res => {
+          expect(res).to.have.status(400);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.property('error');
+          return res.body;
+        });
+    });
+  });
+
+  describe('request update', () => {
+    let carModel = new CarModel();
+    let chaiHttpResponse: Response;
+    let app = server.getApp();
+  
+    before(() => {
+      sinon.stub(carModel.model, 'findOneAndUpdate').resolves(oneCarMock as any);
+    });
+  
+    after(() => {
+      sinon.restore();
+    });
+  
+    it('should return status 200 and an object', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .put(`/cars/${oneCarMock._id}`)
+        .send(oneCarMock)
+        .then(res => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.be.deep.equal(oneCarMock);
+          return res.body;
+      });
+    });
   });
 })
