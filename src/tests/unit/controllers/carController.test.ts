89e7752f0eef95228eea9824d9acf4d98b5396b3
fernandowin <nando_filipe@hotmail.com>
2022-06-23 15:50:13 -0300

    tests carController

--- src/tests/unit/controllers/carController.test.ts
+++ src/tests/unit/controllers/carController.test.ts
@@ -9,95 +9,111 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('request new car', () => {
-  let carModel = new CarModel();
-  let chaiHttpResponse: Response;
-  let app = server.getApp();
-
-  before(() => {
-    sinon.stub(carModel.model, 'create').resolves(oneCarMock);
-  });
-
-  after(() => {
-    sinon.restore();
-  });
-
-  it('should new car', async () => {
-    chaiHttpResponse = await chai
-      .request(app)
-      .post('/cars')
-      .send(newCarMock)
-      .then(res => {
-        expect(res).to.have.status(201);
-        expect(res.body).to.be.an('object');
-        expect(res.body).to.be.deep.equal(oneCarMock);
-        return res.body;
+describe('Test carController', () => {
+  describe('request new car', () => {
+    let carModel = new CarModel();
+    let chaiHttpResponse: Response;
+    let app = server.getApp();
+  
+    before(() => {
+      sinon.stub(carModel.model, 'create').resolves(oneCarMock);
     });
-  });
-
-  it('should return status 400', async () => {
-    chaiHttpResponse = await chai
-      .request(app)
-      .post('/cars')
-      .send({})
-      .then(res => {
-        expect(res).to.have.status(400);
-        expect(res.body).to.be.an('object');
-        expect(res.body).to.have.property('error');
-        return res.body;
+  
+    after(() => {
+      sinon.restore();
+    });
+  
+    it('should new car', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/cars')
+        .send(newCarMock)
+        .then(res => {
+          expect(res).to.have.status(201);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.be.deep.equal(oneCarMock);
+          return res.body;
       });
+    });
+  
+    it('should return status 400', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/cars')
+        .send({})
+        .then(res => {
+          expect(res).to.have.status(400);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.property('error');
+          return res.body;
+        });
+    });
+  
   });
+  
+  describe('request find all', () => {
+    let carModel = new CarModel();
+    let chaiHttpResponse: Response;
+    let app = server.getApp();
+  
+    before(() => {
+      sinon.stub(carModel.model, 'find').resolves(allCarsMock as any[]);
+    });
+  
+    after(() => {
+      sinon.restore();
+    });
+  
+    it('should return status 200 and an array', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/cars')
+        .send(allCarsMock)
+        .then(res => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('array');
+          expect(res.body).to.be.deep.equal(allCarsMock);
+          return res.body;
+      });
+    });
 
-});
-
-describe('request find all', () => {
-  let carModel = new CarModel();
-  let chaiHttpResponse: Response;
-  let app = server.getApp();
-
-  before(() => {
-    sinon.stub(carModel.model, 'find').resolves(allCarsMock as any[]);
-  });
-
-  after(() => {
-    sinon.restore();
-  });
-
-  it('should return status 200 and an array', async () => {
-    chaiHttpResponse = await chai
-      .request(app)
-      .get('/cars')
-      .then(res => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.be.an('array');
-        expect(res.body).to.be.deep.equal(allCarsMock);
-        return res.body;
+    it('should return status 400', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/cars')
+        .send([{}])
+        .then(res => {
+          expect(res).to.have.status(400);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.property('error');
+          return res.body;
+        });
     });
   });
-});
-
-describe('request findOne', () => {
-  let carModel = new CarModel();
-  let chaiHttpResponse: Response;
-  let app = server.getApp();
-
-  before(() => {
-    sinon.stub(carModel.model, 'findOne').resolves(oneCarMock as any);
-  });
-
-  after(() => {
-    sinon.restore();
-  });
-
-  it('should return status 200 and an object', async () => {
-    chaiHttpResponse = await chai
-      .request(app)
-      .get(`/cars/${oneCarMock._id}`)
-      .then(res => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.be.an('object');
-        expect(res.body).to.be.deep.equal(oneCarMock);
-        return res.body;
+  
+  describe('request findOne', () => {
+    let carModel = new CarModel();
+    let chaiHttpResponse: Response;
+    let app = server.getApp();
+  
+    before(() => {
+      sinon.stub(carModel.model, 'findOne').resolves(oneCarMock as any);
+    });
+  
+    after(() => {
+      sinon.restore();
+    });
+  
+    it('should return status 200 and an object', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .get(`/cars/${oneCarMock._id}`)
+        .then(res => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.be.deep.equal(oneCarMock);
+          return res.body;
+      });
     });
   });
-});
+})
