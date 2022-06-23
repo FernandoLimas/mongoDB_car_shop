eac664ed49b3e0c0a1c1e4523ca65ed7ae9bcb99
fernandowin <nando_filipe@hotmail.com>
2022-06-23 15:41:10 -0300

    tests car controller

--- src/tests/unit/controllers/carController.test.ts
+++ src/tests/unit/controllers/carController.test.ts
@@ -34,4 +34,70 @@ describe('request new car', () => {
         return res.body;
     });
   });
+
+  it('should return status 400', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/cars')
+      .send({})
+      .then(res => {
+        expect(res).to.have.status(400);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('error');
+        return res.body;
+      });
+  });
+
+});
+
+describe('request find all', () => {
+  let carModel = new CarModel();
+  let chaiHttpResponse: Response;
+  let app = server.getApp();
+
+  before(() => {
+    sinon.stub(carModel.model, 'find').resolves(allCarsMock as any[]);
+  });
+
+  after(() => {
+    sinon.restore();
+  });
+
+  it('should return status 200 and an array', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/cars')
+      .then(res => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.be.deep.equal(allCarsMock);
+        return res.body;
+    });
+  });
+});
+
+describe('request findOne', () => {
+  let carModel = new CarModel();
+  let chaiHttpResponse: Response;
+  let app = server.getApp();
+
+  before(() => {
+    sinon.stub(carModel.model, 'findOne').resolves(oneCarMock as any);
+  });
+
+  after(() => {
+    sinon.restore();
+  });
+
+  it('should return status 200 and an object', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .get(`/cars/${oneCarMock._id}`)
+      .then(res => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.be.deep.equal(oneCarMock);
+        return res.body;
+    });
+  });
 });
